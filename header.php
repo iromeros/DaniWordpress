863c7d5425b309190870e3cbd8a9c59a61704464
ivan <iromerosanchez9@gmail.com>
2022-05-18 18:10:39 +0200

    a

--- header.php
+++ header.php
@@ -18,5 +18,25 @@
             text-align: center;
         }
     </style>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.html">Navbar básico</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+      aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="<?php echo site_url("blog.php");?>">Blog<span class="sr-only"></span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="<?php echo site_url("galeria.php");?>">Galeria<span class="sr-only"></span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#" tabindex="-1">Nada</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </head>
 <body>
