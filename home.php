ae30366339a04180871351bd947afaeda3e9f090
ivan <iromerosanchez9@gmail.com>
2022-05-30 17:28:54 +0200

    a

--- home.php
+++ home.php
@@ -123,7 +123,16 @@
             <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout.</p>
           </article>
     
-          
+      
+    
+    
+        <div class="col-md-4">
+          <div class="position-sticky" style="top: 2rem;">
+            <div class="p-4 mb-3 bg-light rounded">
+              <h4 class="fst-italic">About</h4>
+              <p class="mb-0">Customize this section to tell your visitors a little bit about your publication, writers, content, or something else entirely. Totally up to you.</p>
+            </div>
+    
             <div class="p-4">
               <h4 class="fst-italic">Archives</h4>
               <ol class="list-unstyled mb-0">
