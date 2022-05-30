76022864bf563761e8ab71d664ca467eb7924b3d
ivan <iromerosanchez9@gmail.com>
2022-05-30 17:27:00 +0200

    a

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
       <div class="nav-scroller py-1 mb-2">
         <nav class="nav d-flex justify-content-between">
           <a class="p-2 link-secondary" href="#">World</a>
-          <a class="p-2 link-secondary" href="#">U.S.</a>z
+          <a class="p-2 link-secondary" href="#">U.S.</a>
           <a class="p-2 link-secondary" href="#">Technology</a>
           <a class="p-2 link-secondary" href="#">Design</a>
           <a class="p-2 link-secondary" href="#">Culture</a>
