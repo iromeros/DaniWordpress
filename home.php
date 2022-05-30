d9d22add5cab4be59197a1148586686c0c540dbd
ivan <iromerosanchez9@gmail.com>
2022-05-30 17:26:41 +0200

    a

--- home.php
+++ home.php
@@ -2,21 +2,7 @@
 ?>
 
 <body>
-    
-    <div class="container">
-      <header class="blog-header lh-1 py-3">
-        <div class="row flex-nowrap justify-content-between align-items-center">
-          <div class="col-4 pt-1">
-            <a class="link-secondary" href="#">Subscribe</a>
-          </div>
-          <div class="col-4 text-center">
-            <a class="blog-header-logo text-dark" href="#">Large</a>
-          </div>
-          <div class="col-4 d-flex justify-content-end align-items-center"></a>
-          </div>
-        </div>
-      </header>
-    
+
       <div class="nav-scroller py-1 mb-2">
         <nav class="nav d-flex justify-content-between">
           <a class="p-2 link-secondary" href="#">World</a>
